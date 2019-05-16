1bf33e8207f440c37fec3df5a090a781063ff82d
Kostas <kostasbudrevicius@gmail.com>
2019-05-16 23:30:57 +0300

    Created end level screen

--- app/Project Delta Struck/Assets/GameMaster.cs
+++ app/Project Delta Struck/Assets/GameMaster.cs
@@ -11,7 +11,11 @@ public class GameMaster : MonoBehaviour {
     public Canvas GameCanvas;
     public CinemachineVirtualCamera Camera;
     public Text moneyTxt;
-
+    public AnimatedCounting EnemiesKilled;
+    public AnimatedCounting CoinsCollected;
+    public GameObject endLevelUI;
+    public EnableQueue EndLevelElements;
+    public GameObject nextLevelBtn;
     // Use this for initialization
     void Awake () {
         Instance = this;
@@ -27,5 +31,24 @@ public class GameMaster : MonoBehaviour {
     {
         Data.Instance.Money += amount;
         moneyTxt.text = Data.Instance.Money.ToString();
+    }
+
+    /// <summary>
+    /// Method to be called when level ends
+    /// </summary>
+    /// <param name="reason">Why the level ended</param>
+    /// <param name="win">Was the level failed or won</param>
+    public void LevelEnded(string reason, bool win)
+    {
+        {
+            nextLevelBtn.SetActive(win);
+            EnemiesKilled.Value = Data.Instance.LevelEnemiesKilled;
+            CoinsCollected.Value = Data.Instance.LevelCoinsCollected;
+            endLevelUI.SetActive(true);
+            EndLevelElements.StartEnabling();
+        }
+        {
+            //Debug.LogWarning("Level ended unexpectedly");
+        }
     }
 }
---
+++ app/Project Delta Struck/Assets/Scripts/EnableQueue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableQueue : MonoBehaviour {
+    public Transform Container;
+    public List<string> AllowedTags = new List<string>();
+    RectTransform[] objects;
+
+	// Use this for initialization
+	void Start ()
+    {
+        objects = Container.GetComponentsInChildren<RectTransform>(true);
+        AllowedTags.Add("EndLevelElement");
+    }
+
+    public void StartEnabling()
+    {
+        StartCoroutine(LateCall());
+    }
+
+
+    IEnumerator LateCall()
+    {
+        foreach (RectTransform r in objects)
+        {
+            if (AllowedTags.Contains(r.tag))
+            {
+                r.gameObject.SetActive(true);
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+        //Do Function here...
+    }
+}
